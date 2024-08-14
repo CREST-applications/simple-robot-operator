03e1dae4aa144cb9473708fb14213e511344bec7
jme-rs <jme.co.jp@gmail.com>
2024-08-14 13:50:43 +0900

    fix

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 from setuptools import find_packages, setup
 
-package_name = 'simple_robot_operator'
+package_name = "simple_robot_operator"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='jme.co.jp@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="root",
+    maintainer_email="al21057@shibaura-it.ac.jp",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             "simple_robot_operator = simple_robot_operator.main:main",
         ],
     },
